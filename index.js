4534fc4549923defd7436589437db047c5817636
Jonah Kroll <114931219+BlueTechDev@users.noreply.github.com>
2025-02-03 12:30:46 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', function () {
+  const accordionHeaders = document.querySelectorAll('.accordion-header');
+
+  accordionHeaders.forEach(header => {
+    header.addEventListener('click', function () {
+      const content = this.nextElementSibling;
+      content.classList.toggle('active');
+    });
+  });
+});
